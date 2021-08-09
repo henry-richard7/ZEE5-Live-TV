380b78a84e0a92d0a989a98fd337dd1f0bed546a
Henry Richard J <henry.richard.2k@gmail.com>
2021-08-09 12:48:52 +0530

    Upload Files

---
+++ ZEE5 Live TV/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace ZEE5_Live_TV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceCodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developedByHenryRichardJToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.donateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paypalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView_Channels = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.donateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sourceCodeToolStripMenuItem,
+            this.developedByHenryRichardJToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // sourceCodeToolStripMenuItem
+            // 
+            this.sourceCodeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.githubToolStripMenuItem});
+            this.sourceCodeToolStripMenuItem.Name = "sourceCodeToolStripMenuItem";
+            this.sourceCodeToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.sourceCodeToolStripMenuItem.Text = "Source Code";
+            // 
+            // githubToolStripMenuItem
+            // 
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.githubToolStripMenuItem.Text = "Github";
+            this.githubToolStripMenuItem.Click += new System.EventHandler(this.githubToolStripMenuItem_Click);
+            // 
+            // developedByHenryRichardJToolStripMenuItem
+            // 
+            this.developedByHenryRichardJToolStripMenuItem.Name = "developedByHenryRichardJToolStripMenuItem";
+            this.developedByHenryRichardJToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.developedByHenryRichardJToolStripMenuItem.Text = "Developed By Henry Richard J";
+            // 
+            // donateToolStripMenuItem
+            // 
+            this.donateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paypalToolStripMenuItem});
+            this.donateToolStripMenuItem.Name = "donateToolStripMenuItem";
+            this.donateToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.donateToolStripMenuItem.Text = "Donate";
+            // 
+            // paypalToolStripMenuItem
+            // 
+            this.paypalToolStripMenuItem.Name = "paypalToolStripMenuItem";
+            this.paypalToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.paypalToolStripMenuItem.Text = "Paypal";
+            this.paypalToolStripMenuItem.Click += new System.EventHandler(this.paypalToolStripMenuItem_Click);
+            // 
+            // listView_Channels
+            // 
+            this.listView_Channels.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(239)))), ((int)(((byte)(244)))));
+            this.listView_Channels.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView_Channels.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView_Channels.HideSelection = false;
+            this.listView_Channels.Location = new System.Drawing.Point(0, 28);
+            this.listView_Channels.Name = "listView_Channels";
+            this.listView_Channels.Size = new System.Drawing.Size(969, 646);
+            this.listView_Channels.TabIndex = 1;
+            this.listView_Channels.UseCompatibleStateImageBehavior = false;
+            this.listView_Channels.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView_Channels_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 674);
+            this.Controls.Add(this.listView_Channels);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ZEE5 Live TV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sourceCodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developedByHenryRichardJToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem donateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paypalToolStripMenuItem;
+        private System.Windows.Forms.ListView listView_Channels;
+    }
+}
+
---
+++ ZEE5 Live TV/Form1.cs
@@ -0,0 +1,160 @@
+﻿using Leaf.xNet;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZEE5_Live_TV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string languages = "ta,kn,pa,bn,en,ml,mr,gu,te,hi";
+        private ArrayList channelList = new ArrayList();
+
+        private string getLiveToken()
+        {
+            HttpRequest tokenRequest = new HttpRequest();
+            string response = tokenRequest.Get("https://useraction.zee5.com/token/live.php").ToString();
+            string token = JObject.Parse(response).SelectToken("video_token").ToString();
+            return token;
+        }
+        private string getToken()
+        {
+            HttpRequest tokenRequest = new HttpRequest();
+            string response = tokenRequest.Get("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app").ToString();
+            string token = JObject.Parse(response).SelectToken("token").ToString();
+            return token;
+        }
+
+        private void getChannels()
+        {
+            int i = 0;
+            string url = "https://catalogapi.zee5.com/v1/channel/bygenre?sort_by_field=channel_number&sort_order=ASC&genres=FREE%20Channels,Hindi%20Entertainment,Hindi%20Movies,English%20Entertainment,Entertainment,Movie,News,Hindi%20News,English%20News,Marathi,Tamil,Telugu,Bengali,Malayalam,Kannada,Punjabi,Kids,Gujarati,Odiya,Music,Lifestyle,Devotional,Comedy,Drama,Sports,Infotainment&country=IN&translation=en&languages="+languages;
+            HttpRequest request = new HttpRequest();
+            string response = request.Get(url).ToString();
+            var token = JObject.Parse(response);
+
+            JArray itemsArray = (JArray)token.SelectToken("items");
+            ImageList dowloadedImages = new ImageList();
+            dowloadedImages.ImageSize = new Size(150, 150);
+            dowloadedImages.ColorDepth = ColorDepth.Depth32Bit;
+
+            foreach (var items in itemsArray)
+            {
+                foreach (var channel in items.SelectToken("items"))
+                {
+
+                    listView_Channels.Items.Add(channel.SelectToken("title").ToString(), i);
+                    WebClient w = new WebClient();
+                    string imageTempUrl = "https://akamaividz2.zee5.com/image/upload/w_386,h_386,c_scale/resources/" + channel.SelectToken("id").ToString() + "/channel_web/" + channel.SelectToken("list_image").ToString();
+                    channelList.Add(channel.SelectToken("id").ToString());
+                    byte[] imageBytes = w.DownloadData(imageTempUrl);
+                    MemoryStream memoryStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                    memoryStream.Write(imageBytes, 0, imageBytes.Length);
+                    Image imgs = Image.FromStream(memoryStream, true);
+                    dowloadedImages.Images.Add(imgs);
+
+                    i++;
+
+                }
+                listView_Channels.LargeImageList = dowloadedImages;
+            }
+        }
+
+        private string getStream(string ChannelID)
+        {
+            HttpRequest streamRequest = new HttpRequest();
+            streamRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36";
+            try
+            {
+                string response = streamRequest.Get("https://catalogapi.zee5.com/v1/channel/" + ChannelID + "?translation=en&country=IN").ToString();
+
+                if (JObject.Parse(response).SelectToken("stream_url_hls") != null)
+                {
+                    string streamUrl = JObject.Parse(response).SelectToken("stream_url_hls").ToString() + getLiveToken();
+                    return streamUrl;
+                }
+                else
+                {
+
+                    return "NO-STREAM";
+                }
+            }
+            catch
+            {
+                return "NO-STREAM";
+            }
+            
+
+
+        }
+
+        private void playUrl(String videoLink)
+        {
+            try
+            {
+
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = @"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe ";
+
+                startInfo.Arguments = videoLink;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            catch
+            {
+
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                startInfo.FileName = Environment.CurrentDirectory + @"\\Default Player\\mpv.exe";
+                startInfo.Arguments = videoLink;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getChannels();
+        }
+
+        private void listView_Channels_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (getStream(channelList[listView_Channels.FocusedItem.Index].ToString()) != "NO-STREAM")
+            {
+                playUrl(getStream(channelList[listView_Channels.FocusedItem.Index].ToString()));
+            }
+            else
+            {
+                MessageBox.Show("No Stream available", "Stream Not Available");
+            }
+        }
+
+        private void githubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/henry-richard7");
+        }
+
+        private void paypalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.paypal.com/paypalme/henryrics");
+        }
+    }
+}
